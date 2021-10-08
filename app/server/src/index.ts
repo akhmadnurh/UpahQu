1a475641afc7120378d36a34ff71c68028b4d8b6
Monokuro <07akhmadnur@gmail.com>
2021-10-09 00:31:03 +0700

    Login session

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,14 +1,6 @@
 const CryptoJS = require("crypto-js");
-
-// Encrypt
-const encrypt = (val) => { return CryptoJS.AES.encrypt(val, 'secret key 123').toString() };
-
-// Decrypt
-const decrypt = (val) => {
-    var bytes  = CryptoJS.AES.decrypt(val, 'secret key 123');
-    return bytes.toString(CryptoJS.enc.Utf8);
-}
+import md5 from "md5";
 
 export const ext = {
-    encrypt, decrypt
+    md5
 }
