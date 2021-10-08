9aa74649904703f3c4c57842c3f2561bf8ff6908
amur13 <18410100030@dinamika.ac.id>
2021-10-08 20:33:26 +0700

    DB
    db tambah dan login

--- app/db/src/index.ts
+++ app/db/src/index.ts
@@ -1,3 +1,18 @@
+const CryptoJS = require("crypto-js");
+
+// Encrypt
+const encrypt = (val) => { CryptoJS.AES.encrypt(val, 'secret key 123').toString()} ;
+
+// Decrypt
+const decrypt = (val) =>{
+  var bytes  = CryptoJS.AES.decrypt(val, 'secret key 123');
+  return bytes.toString(CryptoJS.enc.Utf8);
+  
+}
+export const ext = {
+  encrypt, decrypt
+}
+
 import { main } from "./main";
 
 export const db = main
