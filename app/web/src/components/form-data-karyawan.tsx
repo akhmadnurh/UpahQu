d3b340d19a1f97e646755436c27ec0c1704fcb53
afrisco <afriscoardytia@gmail.com>
2021-10-19 16:21:58 +0700

    input select belum fix widthnya

--- app/web/src/components/form-data-karyawan.tsx
+++ app/web/src/components/form-data-karyawan.tsx
@@ -6,11 +6,13 @@ export default ({ children, title }) => {
   if (title === "Tambah Data Karyawan") {
     var method = "POST";
     var actionn = "";
+    var tombol = "Tambah Karyawan";
   } else {
     var method = "GET";
     var actionn = "";
     var value = "sss";
+    var tombol = "Edit Karyawan";
   }
-  const _component = useComponent("form-data-karyawan","/app/web/src/components/form-data-karyawan",{ title, actionn, method, value });
+  const _component = useComponent("form-data-karyawan","/app/web/src/components/form-data-karyawan",{ title, actionn, method , tombol});
   return eval(_component.render)
 }
