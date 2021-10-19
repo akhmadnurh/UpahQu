7f6fe8213766da8947199f82d76b6c089119d995
afrisco <afriscoardytia@gmail.com>
2021-10-19 19:39:37 +0700

    detail data karyawan dan tabel divisi

--- app/web/src/components/sidebar.tsx
+++ app/web/src/components/sidebar.tsx
@@ -2,24 +2,24 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-
-
 export default ({ children }) => {
   const logout = () => {
     window.user = {};
-    navigate('/login')
-  }
+    navigate("/login");
+  };
 
   const dashboard = () => {
-    navigate('/admin/dashboard')
-  }
+    navigate("/admin/dashboard");
+  };
+
+  const divisi = () => {
+    navigate("/admin/divisi");
+  };
 
   const dataKaryawan = () => {
-    navigate('/admin/data-karyawan')
-  }
-  
-  const _component = useComponent("sidebar","/app/web/src/components/sidebar",{logout,
-dashboard,
-dataKaryawan});
-  return eval(_component.render);
+    navigate("/admin/data-karyawan");
+  };
+
+  const _component = useComponent("sidebar","/app/web/src/components/sidebar",{ logout, dashboard, divisi, dataKaryawan });
+  return eval(_component.render)
 }
