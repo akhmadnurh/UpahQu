b3aafa1ce1fbc1ccec628eb8183af2bab3139328
Monokuro <07akhmadnur@gmail.com>
2021-10-25 23:42:37 +0700

    Fitur tambah karyawan

--- app/web/src/components/form-data-karyawan.tsx
+++ app/web/src/components/form-data-karyawan.tsx
@@ -1,8 +1,18 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
+import { api } from "web.utils/src/api";
 
 export default ({ children, title }) => {
+  let meta = {
+    data: '',
+    action: {}
+  }
+  // get data
+  api('/api/r-departemen').then(res => {
+    meta.data = res.departemen
+  })
+
   if (title === "Tambah Data Karyawan") {
     var method = "POST";
     var actionn = "";
@@ -13,6 +23,6 @@ export default ({ children, title }) => {
     var value = "sss";
     var tombol = "Edit Karyawan";
   }
-  const _component = useComponent("form-data-karyawan","/app/web/src/components/form-data-karyawan",{ title, actionn, method, tombol });
+  const _component = useComponent("form-data-karyawan","/app/web/src/components/form-data-karyawan",{ title, actionn, method, tombol, meta });
   return eval(_component.render)
 }
