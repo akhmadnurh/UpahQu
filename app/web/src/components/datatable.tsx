c051417834363e38a4efd7530f876b245e8d72b8
Afrisco Ardytia Febrian Amarta <afriscoardytia@gmail.com>
2022-01-26 16:27:24 +0700

    tambah parameter di detail karyawan

--- app/web/src/components/datatable.tsx
+++ app/web/src/components/datatable.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import DataTable from 'react-data-table-component'
 
-export default ({ title, column, data ,sort, pagination}) => {
+export default ({ title, column, data ,sort, pagination, noTableHead, dense}) => {
   return (
     <DataTable
       title={title}
       columns={column}
       data={data}
       pagination = {pagination}
+      noTableHead = {noTableHead}
+      dense = {dense}
     />
   )
 }
